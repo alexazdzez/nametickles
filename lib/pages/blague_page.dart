72e9b38604aa1f4f5f4d070f36201057aea785d3
alexandre <alexandre@inertiacreeps.net>
2024-10-29 12:01:31 +0100

    V3

--- lib/pages/blague_page.dart
+++ lib/pages/blague_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:nametickles/blague_model.dart';
 
+import '../gemmes.dart';
+
 class EventPage extends StatefulWidget {
   const EventPage({super.key});
 
@@ -12,6 +14,32 @@ class EventPage extends StatefulWidget {
 
 class _EventPageState extends State<EventPage> {
   CollectionReference eventsRef = FirebaseFirestore.instance.collection("Events");
+  final GemmesManager _gemmesManager = GemmesManager();
+  bool _isUpdating = false; // Verrouillage pour éviter plusieurs clics simultanés
+
+  Future<void> _retirerGemmes(int montant) async {
+    if (_isUpdating) return; // Si déjà en cours, on ignore les clics supplémentaires
+    _isUpdating = true;
+
+    bool success = await _gemmesManager.depenserGemmes(montant);
+    if (success) {
+      // Recharge la valeur depuis Firestore pour s'assurer que la valeur est à jour
+      final uid = FirebaseAuth.instance.currentUser?.uid;
+      if (uid != null) {
+        final userDoc = await FirebaseFirestore.instance.collection('Utilisateurs').doc(uid).get();
+        if (userDoc.exists) {
+          setState(() {
+          });
+        }
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Erreur lors de l'ajout de gemmes")),
+      );
+    }
+
+    _isUpdating = false; // Libère le verrou pour permettre d'autres clics
+  }
 
   Future<void> showDescription(Event eventData) async {
     return showDialog<void>(
@@ -40,6 +68,9 @@ class _EventPageState extends State<EventPage> {
                     'like': eventData.like,
                   });
                 }
+                else{
+                  showLiked(eventData);
+                }
               },
               label: const Text("Like"),
               icon: const Icon(Icons.thumb_up_rounded),
@@ -63,6 +94,52 @@ class _EventPageState extends State<EventPage> {
               },
               label: const Text("Demander suppression"),
               icon: const Icon(Icons.cancel_outlined),
+            ),
+            const SizedBox(width: 20),
+            TextButton(
+              child: const Text("Fermer"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+  Future<void> showLiked(Event eventData) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Vous avez déjà mis un like"),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text("Relike : 12 gemmes"),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            ElevatedButton.icon(
+              onPressed: () async {
+                final FirebaseAuth auth = FirebaseAuth.instance;
+                final User? user = auth.currentUser;
+                String? uid = user?.uid;
+
+                if (uid != null && !eventData.like.contains("${uid}2")){
+                  _retirerGemmes(12);
+                  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+                  eventData.like.add("${uid}2");
+                  await _firestore.collection('Events').doc(eventData.id).update({
+                    'like': eventData.like,
+                  });
+                }
+                Navigator.of(context).pop();
+              },
+              label: const Text("ReLike"),
+              icon: const Icon(Icons.thumb_up_rounded),
             ),
             const SizedBox(width: 20),
             TextButton(
