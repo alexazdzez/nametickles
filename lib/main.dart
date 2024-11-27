e2220ef5627090cbafcc7f67bbd9a2783b5e2d9b
alexandre <alexandre@inertiacreeps.net>
2024-11-27 19:44:31 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -68,7 +68,7 @@ class _MyAppState extends State<MyApp> {
   final UpdateChecker updateChecker = UpdateChecker(
     githubUsername: 'alexazdzez',
     repoName: 'nametickles',
-    currentVersion: '4.0.0',
+    currentVersion: '4.1.0',
   );
   final navigatorKey = GlobalKey<NavigatorState>();
 
